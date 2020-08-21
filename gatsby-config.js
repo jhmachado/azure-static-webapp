db16e8d1f6d137a5641f2ee8f16fb174eb597f45
Jhonatan Machado <jh.machado12@gmail.com>
2020-08-21 20:00:13 -0300

    Update web app informations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Azure Static Web Apps`,
+    description: `A simple static web app created to be hosted at azure.`,
+    author: {
+      name: 'Jhonatan Machado',
+      bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+      photo: 'https://api.adorable.io/avatars/285/abott@adorable.png',
+      contacts: {
+        facebook: 'https://facebook.com',
+        linkedin: 'https://linkedin.com',
+        github: 'https://github.com',
+        telegram: 'https://telegram.com',
+        email: 'email@email.com'
+      }
+    },
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
