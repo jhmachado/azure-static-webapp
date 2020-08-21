a75213d2e1005eb1819c9e5d6f245ba112fca778
Jhonatan Machado <jh.machado12@gmail.com>
2020-08-21 20:05:28 -0300

    Refacting the home page with the new styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
+import { Link } from 'gatsby';
 
-export default ({ data }) => (
+export default () => (
   <React.Fragment>
-    <h2>{data.site.siteMetadata.title}</h2>
-    <div>{data.site.siteMetadata.description}</div>
     <Link to="/">Back to home</Link>
   </React.Fragment>
 )
-
-export const query = graphql `
-  query {
-    site {
-      siteMetadata {
-        author,
-        description,
-        title
-      }
-    }
-  }
-`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,47 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Sidebar from "../components/sidebar";
+import { useSiteMetadata } from '../hooks'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+function IndexPage() {
+  const { author } = useSiteMetadata();
+  return (
+    <Layout>
+      <Sidebar author={author} />
+      <SEO author={author} title="Home" />
+      <div className={'page'}>
+        <div className={'page__inner'}>
+          <div className={'page__body'}>
+
+            <div className={'feed'}>
+              <div className={'feed__item'}>
+                <div className={'feed__item-meta'}>
+                  <time className={'feed__item-meta-time'}>20/08/2020</time>
+                  <span className={'feed__item-meta-divider'} />
+                  <span className={'feed__item-meta-category'}>
+                    <Link to={'/random'} className={'feed__item-meta-category-link'}> Random </Link>
+                  </span>
+                </div>
+
+                <h2 className={'feed__item-title'}>
+                  <Link className={'feed__item-title-link'} to={'/'}>
+                    Humane Typography in the Digital Age
+                  </Link>
+                </h2>
+                <p className={'feed__item-description'}>
+                  An Essay on Typography by Eric Gill takes the reader back to the year 1930. The year when a conflict between two worlds came to its term. The machines of the industrial world finally took over the handicrafts.
+                </p>
+                <Link className={'feed__item-readmore'} to={'/'}>Read</Link>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
 
 export default IndexPage
